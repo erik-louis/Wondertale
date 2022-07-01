80a97b96e8f3f491fdf489c89f6f3f86d6a66443
Erik <eriklustenberger90@gmail.com>
2022-07-01 16:04:47 +0200

    Added Main Menu and Audio Manager

---
+++ Wondertale/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+            s.source.outputAudioMixerGroup = s.mix;
+        }
+    }
+
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            Play(sounds[0].clip.name);
+        }
+
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound " + name + " not found!");
+            return;
+        }
+
+        s.source.Play();
+
+        // simply add the following code in any script: FindObjectOfType<AudioManager>().Play("AudioFileName");
+    }
+
+
+    public void StopPlaying(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound " + name + " not found!");
+            return;
+        }
+
+        s.source.Stop();
+
+        // simply add the following code in any script: FindObjectOfType<AudioManager>().StopPlaying("AudioFileName");
+    }
+
+
+}

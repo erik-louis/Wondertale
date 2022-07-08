d731fa903405c92b651ce7f33d833d56a814a903
Erik <eriklustenberger90@gmail.com>
2022-07-08 12:50:58 +0200

    Updates to Dialogue System

--- Wondertale/Assets/Scripts/DialogueSystem/DialogueManager.cs
+++ Wondertale/Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -6,6 +6,7 @@ public class DialogueManager : MonoBehaviour
 {
     public TextMeshProUGUI speakerName, dialogue, navButtonText;
     public Image speakerSprite;
+    public GameObject speakerSpriteObject;
 
     private int currentIndex;
     private Conversation currentConvo;
@@ -38,13 +39,21 @@ public class DialogueManager : MonoBehaviour
 
     public void ReadNext()
     {
+        if (currentIndex > currentConvo.GetLength())
+        {
+            return;
+        }
+
         speakerName.text = currentConvo.GetLineByIndex(currentIndex).speaker.GetName();
         dialogue.text = currentConvo.GetLineByIndex(currentIndex).dialogue;
         speakerSprite.sprite = currentConvo.GetLineByIndex(currentIndex).speaker.GetSprite();
         if (speakerName.text == "Director")
         {
-            Debug.Log("Test");
-            speakerSprite.transform.position = new Vector3(510, 101, 0);
+            speakerSpriteObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(630, 185, 0);
+        }
+        else
+        {
+            speakerSpriteObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(-645, 194, 0);
         }
         currentIndex++;
     }

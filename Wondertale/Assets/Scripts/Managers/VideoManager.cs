8c5c572bd032e3029a3cf50b1ad68f6ef45d4c0a
Erik <eriklustenberger90@gmail.com>
2022-07-29 13:10:48 +0200

    Added Cutscene and Black Intro Scene

---
+++ Wondertale/Assets/Scripts/Managers/VideoManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VideoManager : MonoBehaviour
+{
+    [SerializeField] int currentLevelIndex;
+    private bool loadingStarted = false;
+    private float secondsLeft = 0;
+    public float timer;
+
+    private void Awake()
+    {
+        currentLevelIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+    void Start()
+    {
+        StartCoroutine(DelayLoadLevel(timer));
+    }
+
+    IEnumerator DelayLoadLevel(float seconds)
+    {
+        secondsLeft = seconds;
+        loadingStarted = true;
+
+        do
+        {
+            yield return new WaitForSeconds(1);
+        } 
+        while (--secondsLeft > 0);
+
+        SceneManager.LoadScene(currentLevelIndex +=1);
+    }
+
+    void OnGUI()
+    {
+        if (loadingStarted)
+            GUI.Label(new Rect(0, 0, 100, 20), secondsLeft.ToString());
+    }
+    
+    
+}
